1f6c4527e5d7d58b5d7bdcddc03c3a544c21d724
larrykas <larrykas@users.noreply.github.com>
2013-11-12 10:25:44 +0300

    Update atm.rb

--- atm.rb
+++ atm.rb
@@ -1,11 +1,14 @@
+require 'mysql'
+
 class Account
-  def initialize(balance, name, account_number, con)
+  def initialize(balance, name, job, account_number)
     @balance = balance
     @name = name
+    @job = job
     @account_number = account_number
     
     @con = Mysql.new 'localhost', 'root', '**********', 'banking_system'
-    @co = con.prepare("INSERT INTO clients(account_number, name, job, gender, balance)")  
+   
   end
   def deposit(money)
       bal = balance()
@@ -20,27 +23,10 @@ end
 # I can't get Just this little code to work.
 
 ############################################################################################
-code with error messages every time I cahnge something I get a differrent kind of error
+code with a new message, it looks like the new account was sucessfully created
 ############################################################################################
 Larry$ irb
 irb(main):001:0> load './atm.rb'
 => true
-irb(main):002:0> a = Account.new(011238, "Larry", "CEO", "Male", 5000)
-SyntaxError: (irb):2: Invalid octal digit
-a = Account.new(011238, "Larry", "CEO", "Male", 5000)
-                      ^
-	from /Users/Larry/.rbenv/versions/2.0.0-p247/bin/irb:12:in `<main>'
-irb(main):003:0> a = Account.new(1238, "Larry", "CEO", "Male", 5000)
-ArgumentError: wrong number of arguments (5 for 4)
-	from /Users/Larry/Desktop/projects/ruby-project-13/atm.rb:2:in `initialize'
-	from (irb):3:in `new'
-	from (irb):3
-	from /Users/Larry/.rbenv/versions/2.0.0-p247/bin/irb:12:in `<main>'
-irb(main):004:0> a = Account.new(1238, "Larry", "CEO", 5000)
-NameError: uninitialized constant Account::Mysql
-	from /Users/Larry/Desktop/projects/ruby-project-13/atm.rb:7:in `initialize'
-	from (irb):4:in `new'
-	from (irb):4
-	from /Users/Larry/.rbenv/versions/2.0.0-p247/bin/irb:12:in `<main>'
-irb(main):005:0> 
-
+irb(main):002:0> a = Account.new(3500, "Larry", "CEO", 11238)
+=> #<Account:0x007fc3b1ab8fc0 @balance=3500, @name="Larry", @job="CEO", @account_number=11238, @con=#<Mysql:0x007fc3b1ab8e08 @fields=nil, @protocol=#<Mysql::Protocol:0x007fc3b1ab8cf0 @insert_id=0, @warning_count=0, @gc_stmt_queue=[], @state=:READY, @read_timeout=nil, @write_timeout=nil, @sock=#<UNIXSocket:fd 7>, @authinfo=["root", "vanniekerk", "banking_system", 0, nil], @seq=3, @sqlstate="00000", @server_info="5.6.13", @server_version=50613, @thread_id=50, @charset=#<Mysql::Charset:0x007fc3b184aea0 @csname="utf8_general_ci", @name="utf8", @number=33, @unsafe=false>>, @charset=#<Mysql::Charset:0x007fc3b184aea0 @csname="utf8_general_ci", @name="utf8", @number=33, @unsafe=false>, @connect_timeout=nil, @read_timeout=nil, @write_timeout=nil, @init_command=nil, @sqlstate="00000", @query_with_result=true, @host_info="Localhost via UNIX socket", @last_error=nil, @result_exist=false, @local_infile=nil>>
