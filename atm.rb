34056fa7362d27744f89bb80390ee433597828f6
larrykas <larrykas@users.noreply.github.com>
2013-11-15 19:58:32 +0300

    Update atm.rb

--- atm.rb
+++ atm.rb
@@ -1,4 +1,5 @@
-require 'mysql'
+
+ require 'mysql'
 class Account
   def initialize(account_number, name, job, gender, balance)
     @account_number = account_number
@@ -7,14 +8,27 @@ class Account
     @gender = gender
     @balance = balance
     
-    @con = Mysql.new 'localhost', 'root', '**********', 'banking_system'
-    con.query("INSERT INTO clients(account_number, name, job, gender, balance)VALUES(?, ?, ?, ?)")  
   end
-  def deposit(money)
+   
+  con = Mysql.new('localhost', 'root', 'vanniekerk', 'banking_system');
+  con.query("CREATE TABLE IF NOT EXISTS \ Accounts(account_number INT PRIMARY KEY, name VARCHAR(50), job VARCHAR(25), gender VARCHAR(6), balance INT(25))");
+  st = con.prepare("INSERT INTO Accounts(account_number, name, job, gender, balance)VALUES(?, ?, ?, ?, ?)")
+  st.execute(11239, 'Larry', 'Pro', 'male', 3400)
+  st.execute(11240, 'Sedy', 'Analyst', 'male', 2400)
+  st.execute(11241, 'Cheryl', 'Accountant', 'female', 2400)
+  st.close  
+  con.commit
+   
+   
+  def insert(dto) 
+    
+  end
+  
+  def deposit(amount)
       bal = balance()
       num = bal.to_f
       num += money
-      st = @db.query("UPDATE accounts SET balance = '#{@balance}'  WHERE name = '#{@name}'")
+      st = @db.query("UPDATE Accounts SET balance = '#{@balance}'  WHERE name = '#{@name}'")
       puts bal
   end
 
@@ -40,13 +54,3 @@ class Account
     return @balance
   end
 end
-
-
-############################################################################################
-code with a new message, it looks like the new account was sucessfully created
-############################################################################################
-Larry$ irb
-irb(main):001:0> load './atm.rb'
-=> true
-irb(main):002:0> a = Account.new(11238, "Larry", "CIO", "Male", 3400)
-=> #<Account:0x007faadd9e7e08 @account_number=11238, @name="Larry", @job="CIO", @gender="Male", @balance=3400, @con=#<Mysql:0x007faadd9e7c50 @fields=nil, @protocol=#<Mysql::Protocol:0x007faadd9e7b88 @insert_id=0, @warning_count=0, @gc_stmt_queue=[], @state=:READY, @read_timeout=nil, @write_timeout=nil, @sock=#<UNIXSocket:fd 7>, @authinfo=["root", "**********", "banking_system", 0, nil], @seq=3, @sqlstate="00000", @server_info="5.6.13", @server_version=50613, @thread_id=56, @charset=#<Mysql::Charset:0x007faadc048a60 @csname="utf8_general_ci", @name="utf8", @number=33, @unsafe=false>>, @charset=#<Mysql::Charset:0x007faadc048a60 @csname="utf8_general_ci", @name="utf8", @number=33, @unsafe=false>, @connect_timeout=nil, @read_timeout=nil, @write_timeout=nil, @init_command=nil, @sqlstate="00000", @query_with_result=true, @host_info="Localhost via UNIX socket", @last_error=nil, @result_exist=false, @local_infile=nil>>
