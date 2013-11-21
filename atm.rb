aa3e2a1f455d4d0f4c9414c50f1ef309847f5581
Larry Kasaija <Larry@Larrys-MacBook-Air.local>
2013-11-21 20:57:53 +0300

    Made a quick change

--- atm.rb
+++ atm.rb
@@ -10,7 +10,7 @@ class Account
     @balance = balance
     @con = Mysql.new('localhost', 'root', '**********', 'banking_system');
     begin 
-      
+      # Hello World 
       #con.query("CREATE TABLE IF NOT EXISTS \ Accounts(account_number INT PRIMARY KEY, name VARCHAR(50), job VARCHAR(25), gender VARCHAR(6), balance INT(25))");
       st = con.prepare("INSERT INTO Accounts(account_number, name, job, gender, balance)VALUES(?, ?, ?, ?, ?)")
       st.execute(11239, 'Larry', 'Pro', 'male', 3400)
